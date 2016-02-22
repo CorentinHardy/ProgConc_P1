70c465a0c646d70cb23507a5da076e59c0456a7d
Corentin <corentin.fouyaya@hotmail.fr>
2016-02-22 09:13:51 +0100

    [Updated] méthode inutile

--- src/Chaleur.c
+++ src/Chaleur.c
@@ -270,19 +270,14 @@ static void transfert(float** grilleResult, int x, int y, float value) {
 static float taylor(int x, int y){
   return 0.0;
 }
-
-static float val22 = 4.5;
-static float val12 = 1/9;
-static float val11 = 1/36;
-
+/*
  * on calcule en considérant une matrice dont on a les 9 valeurs proches,
- * on connait aussi les coefficients à appliquer, avec val11=val13=val31=val33
- * et val12=val21=val23=val32.
- 
+ * on connait aussi les coefficients à appliquer, avec COEF_ANGLE=val11=val13=val31=val33
+ * et COEF_BORD=val12=val21=val23=val32.
+ * /
 static float cellule_9(float** tab_9, int x, int y){
-  return val22 * tab_9[x][y] + val12 * (tab_9[x+1][y] + tab_9[x][y+1] 
-    + tab_9[x-1][y] + tab_9[x][y-1]) + val11 * (tab_9[x+1][y+1] 
+  return COEF_CENTRE * tab_9[x][y] + COEF_BORD * (tab_9[x+1][y] + tab_9[x][y+1] 
+    + tab_9[x-1][y] + tab_9[x][y-1]) + COEF_ANGLE * (tab_9[x+1][y+1] 
     + tab_9[x-1][y+1] + tab_9[x-1][y-1] + tab_9[x+1][y-1]);
 }
-
-*/
+//*/
